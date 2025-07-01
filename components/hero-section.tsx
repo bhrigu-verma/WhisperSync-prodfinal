13c58749c49b5473e1a6e09e4b4ce644ef9827e1
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2025-07-01 16:07:42 +0530

    hero section done 111

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,11 +3,15 @@ import { Cover } from "@/components/ui/cover";
 const HeroSection = () => {
     return (
       <div className="relative text-center h-[300px] sm:h-[200px] flex flex-col justify-center items-center">
-        <h1 className="text-3xl sm:text-5xl text-white mb-4 sm:mb-6 font-extrabold tracking-tight leading-tight">
-          Transform Your Videos with{" "}
-          <span className="text-yellow-400">AI-Powered Transcriptions</span>
-        </h1>
-        <p className="text-base sm:text-lg text-gray-100 max-w-2xl mx-auto font-medium leading-relaxed">
+        {/* <h1 className="text-3xl sm:text-5xl text-white mb-4 sm:mb-6 font-extrabold tracking-tight leading-tight">
+          Turn Speech into Storytelling.{" "}
+          <span className="text-yellow-400">with WhisperSync</span>
+        </h1> */}
+
+         <h1 className="text-4xl md:text-5xl pd-5 lg:text-7xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        Turn Speech into Storytelling.<br /> at <Cover className="text-yellow-400">WhisperSync</Cover>
+         </h1>
+        <p className="text-base sm:text-lg text-gray-100 max-w-2xl mb-16 mx-auto  m-6 font-medium leading-loose p-5">
           Upload your short-form videos and receive precise transcriptions in
           minutes. Built for content creators, editors, and social media
           managers.
