785b5b0fe5974118598359137e6bc74fe5912a20
Bhrigu verma <146697095+bhrigu-verma@users.noreply.github.com>
2025-07-05 10:46:36 +0530

    some imp chnages to prod

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,29 +1,29 @@
-// "use client"
-// import { cn } from "@/lib/utils"
-// import { Button } from "@/components/ui/button"
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
-// import { signIn } from "next-auth/react"
-// import Link from "next/link"
-// import Image from "next/image"
+"use client"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { signIn } from "next-auth/react"
+import Link from "next/link"
+import Image from "next/image"
 
-// const  SignUp = () => {
-//   return (
-//     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-[#13171d]">
-//     <div className="w-full max-w-sm">
-//     <div className={cn("flex flex-col gap-6 ")}>
-//       <Card className="bg-[#1c2128]  text-white">
-//         <CardHeader>
-//           <CardTitle className="text-2xl">Login</CardTitle>
-//           <CardDescription>
-//             Enter your email below to login to your account
-//           </CardDescription>
-//         </CardHeader>
+const  SignUp = () => {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-[#13171d]">
+    <div className="w-full max-w-sm">
+    <div className={cn("flex flex-col gap-6 ")}>
+      <Card className="bg-[#1c2128]  text-white">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
         <CardContent> 
             <div className="flex flex-col gap-y-4 mt-4">
               <Button 
